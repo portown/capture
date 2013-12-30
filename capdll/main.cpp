a1156eec8166609f51a91637be0c03e5b954152b
YASUDA Jiro <portown09@gmail.com>
2013-12-30 15:05:24 +0900

    Fix coding styles

--- capdll/main.cpp
+++ capdll/main.cpp
@@ -1,93 +1,86 @@
 // main.cpp
 
-// 結合
 #include <windows.h>
 #include "capdll.h"
 
 // 変数の宣言
 #pragma data_seg( "CAP_DATA" )
 HHOOK hMouseHook = 0;
-HWND hMse = 0;
-UINT uType = 0;
+HWND  hMse       = 0;
+UINT  uType      = 0;
 #pragma data_seg()
-HINSTANCE hInst;
 
+namespace
+{
+  HINSTANCE hInst;
+}
 
-// ==============================================
-// 実装
-// ==============================================
 
 // エントリポイント
-BOOL WINAPI DllMain( HINSTANCE hCurInst, DWORD dwReason, LPVOID lpReserved )
+BOOL WINAPI DllMain(HINSTANCE hCurInst, DWORD dwReason, LPVOID)
 {
-	switch ( dwReason )
-	{
-		case DLL_PROCESS_ATTACH:
-			hInst = hCurInst;
-			break;
-	}
+  switch (dwReason)
+  {
+    case DLL_PROCESS_ATTACH:
+      hInst = hCurInst;
+      break;
+  }
 
-	return TRUE;
+  return TRUE;
 }
 
 // マウスフック開始
-int SetMouseHook( HWND hDst, UINT uTp )
+EXPORT int SetMouseHook(HWND hDst, UINT uTp)
 {
-	if ( uTp > SMH_MAX )
-		return 0;
+  if (uTp > SMH_MAX)
+    return 0;
 
-	hMouseHook = SetWindowsHookEx( WH_MOUSE, MouseHookProc, hInst, 0 );
-	if ( !hMouseHook )
-		return 0;
+  hMouseHook = SetWindowsHookEx(WH_MOUSE, MouseHookProc, hInst, 0);
+  if (!hMouseHook)
+    return 0;
 
-	hMse = hDst;
-	uType = uTp;
+  hMse  = hDst;
+  uType = uTp;
 
-	return 1;
+  return 1;
 }
 
 // マウスフック検査
-bool IsMouseHooking( void )
+EXPORT bool IsMouseHooking()
 {
-	return ( hMouseHook != NULL );
+  return (hMouseHook != nullptr);
 }
 
 // マウスフック削除
-int ResetMouseHook( void )
+EXPORT int ResetMouseHook()
 {
-	if ( !UnhookWindowsHookEx( hMouseHook ) )
-		return 0;
+  if (!UnhookWindowsHookEx(hMouseHook))
+    return 0;
 
-	hMouseHook = NULL;
+  hMouseHook = nullptr;
 
-	return 1;
+  return 1;
 }
 
 // マウスフックプロシージャ
-LRESULT CALLBACK MouseHookProc( int nCode, WPARAM wp, LPARAM lp )
+EXPORT LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wp, LPARAM lp)
 {
-	LPMOUSEHOOKSTRUCT lpmh;
-	POINT pt;
+  if (nCode < 0)
+    return CallNextHookEx(hMouseHook, nCode, wp, lp);
 
-	if ( nCode < 0 )
-		return CallNextHookEx( hMouseHook, nCode, wp, lp );
+  LPMOUSEHOOKSTRUCT lpmh = reinterpret_cast<LPMOUSEHOOKSTRUCT>(lp);
+  POINT             pt   = lpmh->pt;
+  SendMessage(hMse, WM_USER, wp, reinterpret_cast<LPARAM>(&pt));
 
-	lpmh = ( LPMOUSEHOOKSTRUCT )lp;
-	pt = lpmh->pt;
-	SendMessage( hMse, WM_USER, wp, ( LPARAM )&pt );
+  switch (uType)
+  {
+    case SMH_SHARE:
+      return CallNextHookEx(hMouseHook, nCode, wp, lp);
 
-	switch ( uType )
-	{
-		case SMH_SHARE:
-			return CallNextHookEx( hMouseHook, nCode, wp, lp );
+    case SMH_EXCLUDE:
+      return TRUE;
 
-		case SMH_EXCLUDE:
-			return TRUE;
-
-		default:
-			return CallNextHookEx( hMouseHook, nCode, wp, lp );
-	}
+    default:
+      return CallNextHookEx(hMouseHook, nCode, wp, lp);
+  }
 }
-
-
-// EOF
--- capture/menu.cpp
+++ capture/menu.cpp
@@ -1,131 +1,122 @@
 // menu.cpp
 
-
-// 結合
 #include "common.h"
 
-// 定数の定義
-#define LISTMAX 54
 
-// 関数の宣言
-INT_PTR OptionProc( HWND, UINT, WPARAM, LPARAM );
-INT_PTR AboutProc( HWND, UINT, WPARAM, LPARAM );
-int SetCombo( HWND );
+namespace
+{
+  constexpr auto LISTMAX = 54;
 
+  INT_PTR CALLBACK OptionProc(HWND, UINT, WPARAM, LPARAM);
+  INT_PTR CALLBACK AboutProc(HWND, UINT, WPARAM, LPARAM);
+  int     SetCombo(HWND);
+}
 
-// ==============================================
-// 実装
-// ==============================================
 
 // オプションダイアログ表示
-int ShowOption( HWND hWnd )
+int ShowOption(HWND hWnd)
 {
-	HINSTANCE hInst;
-	INT_PTR iRet;
+  HINSTANCE hInst = reinterpret_cast<HINSTANCE>(GetWindowLongPtr(hWnd, GWLP_HINSTANCE));
 
-	hInst = ( HINSTANCE )GetWindowLongPtr( hWnd, GWLP_HINSTANCE );
-	iRet = DialogBoxParam( hInst, "IDD_OPTION", hWnd, ( DLGPROC )OptionProc, NULL );
+  DialogBoxParam(hInst, "IDD_OPTION", hWnd, OptionProc, 0);
 
-	return 1;
-}
-
-// オプションダイアログプロシージャ
-INT_PTR OptionProc( HWND hDlg, UINT msg, WPARAM wp, LPARAM lp )
-{
-	switch ( msg )
-	{
-		case WM_INITDIALOG:
-			SetCombo( hDlg );
-			Button_SetCheck( GetDlgItem( hDlg, IDC_CAPCTRL ), bCapCtrl );
-			Button_SetCheck( GetDlgItem( hDlg, IDC_CAPSHIFT ), bCapShift );
-			Button_SetCheck( GetDlgItem( hDlg, IDC_CAPALT ), bCapAlt );
-			break;
-
-		case WM_COMMAND:
-			switch ( wp )
-			{
-				case IDOK:
-					nCapKey = ComboBox_GetCurSel( GetDlgItem( hDlg, IDC_CAPKEY ) );
-					bCapCtrl = Button_GetCheck( GetDlgItem( hDlg, IDC_CAPCTRL ) );
-					bCapShift = Button_GetCheck( GetDlgItem( hDlg, IDC_CAPSHIFT ) );
-					bCapAlt = Button_GetCheck( GetDlgItem( hDlg, IDC_CAPALT ) );
-					EndDialog( hDlg, IDOK );
-					break;
-
-				case IDCANCEL:
-					EndDialog( hDlg, IDCANCEL );
-					break;
-
-				default:
-					return FALSE;
-			}
-			break;
-
-		default:
-			return FALSE;
-	}
-
-	return TRUE;
-}
-
-// コンボボックスのセット
-int SetCombo( HWND hDlg )
-{
-	LPCTSTR szList[LISTMAX] = { "ESCAPE", "SPACE", "PAGEDOWN", "PAGEUP", "END", "HOME",
-		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
-		"G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-		"W", "X", "Y", "Z", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
-		"F11", "F12" };
-	HWND hCap;
-	int i;
-
-	hCap = GetDlgItem( hDlg, IDC_CAPKEY );
-
-	for ( i = 0; i < LISTMAX; i++ )
-		ComboBox_InsertString( hCap, i, szList[i] );
-
-	ComboBox_SetCurSel( hCap, nCapKey );
-
-	return 1;
+  return 1;
 }
 
 // バージョン情報ダイアログ表示
-int ShowAbout( HWND hWnd )
+int ShowAbout(HWND hWnd)
 {
-	HINSTANCE hInst;
+  HINSTANCE hInst = reinterpret_cast<HINSTANCE>(GetWindowLongPtr(hWnd, GWLP_HINSTANCE));
+  DialogBox(hInst, "IDD_ABOUT", hWnd, OptionProc);
 
-	hInst = ( HINSTANCE )GetWindowLongPtr( hWnd, GWLP_HINSTANCE );
-	DialogBox( hInst, "IDD_ABOUT", hWnd, ( DLGPROC )OptionProc );
-
-	return 1;
-}
-
-// バージョン情報ダイアログプロシージャ
-INT_PTR AboutProc( HWND hDlg, UINT msg, WPARAM wp, LPARAM lp )
-{
-	switch ( msg )
-	{
-		case WM_INITDIALOG:
-			break;
-
-		case WM_COMMAND:
-			switch ( wp )
-			{
-				case IDOK:
-					EndDialog( hDlg, IDOK );
-					break;
-
-				default:
-					return FALSE;
-			}
-			break;
-
-		default:
-			return FALSE;
-	}
-
-	return TRUE;
+  return 1;
 }
 
 
-// EOF
+namespace
+{
+  // オプションダイアログプロシージャ
+  INT_PTR CALLBACK OptionProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM)
+  {
+    switch (msg)
+    {
+      case WM_INITDIALOG:
+        SetCombo(hDlg);
+        Button_SetCheck(GetDlgItem(hDlg, IDC_CAPCTRL), bCapCtrl);
+        Button_SetCheck(GetDlgItem(hDlg, IDC_CAPSHIFT), bCapShift);
+        Button_SetCheck(GetDlgItem(hDlg, IDC_CAPALT), bCapAlt);
+        break;
+
+      case WM_COMMAND:
+        switch (wp)
+        {
+          case IDOK:
+            nCapKey   = ComboBox_GetCurSel(GetDlgItem(hDlg, IDC_CAPKEY));
+            bCapCtrl  = Button_GetCheck(GetDlgItem(hDlg, IDC_CAPCTRL));
+            bCapShift = Button_GetCheck(GetDlgItem(hDlg, IDC_CAPSHIFT));
+            bCapAlt   = Button_GetCheck(GetDlgItem(hDlg, IDC_CAPALT));
+            EndDialog(hDlg, IDOK);
+            break;
+
+          case IDCANCEL:
+            EndDialog(hDlg, IDCANCEL);
+            break;
+
+          default:
+            return FALSE;
+        }
+        break;
+
+      default:
+        return FALSE;
+    }
+
+    return TRUE;
+  }
+
+  // コンボボックスのセット
+  int SetCombo(HWND hDlg)
+  {
+    LPCTSTR szList[LISTMAX] = { "ESCAPE", "SPACE", "PAGEDOWN", "PAGEUP", "END", "HOME",
+      "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F",
+      "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+      "W", "X", "Y", "Z", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
+      "F11", "F12" };
+
+    HWND hCap = GetDlgItem(hDlg, IDC_CAPKEY);
+
+    for (int i = 0; i < LISTMAX; i++)
+      ComboBox_InsertString(hCap, i, szList[i]);
+
+    ComboBox_SetCurSel(hCap, nCapKey);
+
+    return 1;
+  }
+
+  // バージョン情報ダイアログプロシージャ
+  INT_PTR CALLBACK AboutProc(HWND hDlg, UINT msg, WPARAM wp, LPARAM)
+  {
+    switch (msg)
+    {
+      case WM_INITDIALOG:
+        break;
+
+      case WM_COMMAND:
+        switch (wp)
+        {
+          case IDOK:
+            EndDialog(hDlg, IDOK);
+            break;
+
+          default:
+            return FALSE;
+        }
+        break;
+
+      default:
+        return FALSE;
+    }
+
+    return TRUE;
+  }
+}
