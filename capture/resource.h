c9a4527f59ef2bc265795c60a42ee29af7f39c59
YASUDA Jiro <portown09@gmail.com>
2014-01-04 03:50:50 +0900

    Change resource encoding

--- capture/resource.h
+++ capture/resource.h
@@ -1,29 +1,20 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by main.rc
-//
+// resource.h
+
+#pragma once
+
 #define IDI_CAPTURE                     5
+
 #define IDR_MAIN                        101
 #define IDR_MENU1                       102
 #define IDR_TAB                         104
-#define IDI_ICON1                       106
+
 #define IDC_CAPKEY                      1002
 #define IDC_CAPCTRL                     1003
 #define IDC_CAPALT                      1004
 #define IDC_CAPSHIFT                    1005
+
 #define IDM_SAVE                        40003
 #define IDM_EXIT                        40004
 #define IDM_ABOUT                       40006
 #define IDM_OPTION                      40008
 #define IDM_CLOSE                       40011
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
-#define _APS_NEXT_COMMAND_VALUE         40012
-#define _APS_NEXT_CONTROL_VALUE         1006
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
