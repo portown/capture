06c7cd3aca6a17f00b1f3c14156a0d16f611ebad
YASUDA Jiro <portown09@gmail.com>
2014-01-28 01:52:00 +0900

    constexpr-ize

--- capture/main.cpp
+++ capture/main.cpp
@@ -27,7 +27,7 @@ BOOL bCapAlt;
 
 namespace
 {
-  int Keys[] =          // キー一覧
+  constexpr int Keys[] =          // キー一覧
   { VK_ESCAPE, VK_SPACE, VK_NEXT, VK_PRIOR, VK_END, VK_HOME,
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
     'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
